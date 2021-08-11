8c4d2a157f68f419f142294c272be9cfd4d2da58
SAkkaya <sadikakkaya.sa@gmail.com>
2021-08-11 10:33:05 +0200

    EU-5 Cucumber final branch 2

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -20,7 +20,7 @@ public class Driver {
     private static WebDriver driver;
 
     public static WebDriver get() {
-        // Test branch deneme
+        // Test 2
         if (driver == null) {
             // this line will tell which browser should open based on the value from properties file
             String browser = ConfigurationReader.get("browser");
